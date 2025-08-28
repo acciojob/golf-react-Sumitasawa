17e0be6054e23cd62cd6f378118ef59ab5011224
Sumitasawa <sumitasawa65@gmail.com>
2025-08-28 08:43:26 +0530

    Golf Game

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,11 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+         this.handleKeyDown = this.handleKeyDown.bind(this);
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall:true})
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -24,10 +25,26 @@ class App extends Component {
 		}
     }
 
+     handleKeyDown(event) {
+    if (event.keyCode === 39 && this.state.renderBall) {
+      this.setState((prevState) => {
+        const newPos = prevState.posi + 5;
+        return {
+          posi: newPos,
+          ballPosition: { left: newPos + "px" }
+        };
+      });
+    }
+  }
+
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+      document.addEventListener("keydown", this.handleKeyDown);
     }
+
+      componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
 
     render() {
         return (
